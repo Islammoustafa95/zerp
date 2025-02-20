91f39b5d75babfed5e49e274ba3903b68d7ec233
islammoustafa95 <moustafa.imym@gmail.com>
2025-02-20 22:33:15 +0200

    site6

--- zerp/www/subscribe.py
+++ zerp/www/subscribe.py
@@ -1,6 +1,5 @@
 import frappe
 from frappe import _
-from datetime import date, timedelta
 
 def get_context(context):
     context.subscription_plans = get_subscription_plans()
@@ -12,19 +11,33 @@ def get_context(context):
 
 def get_subscription_plans():
     # Fetch all subscription plans
-    plans = frappe.get_all("Subscription Plan", fields=["name"])
+    plans = frappe.get_all(
+        "Subscription Plan", 
+        fields=["name", "plan_name", "plan_monthly_subscription", "plan_description"]
+    )
     
-    # Get complete doc for each plan
+    # Get apps for each plan
     plan_docs = []
     for plan in plans:
+        # Fetch the full document to get child table
         plan_doc = frappe.get_doc("Subscription Plan", plan.name)
+        
+        # Prepare plan data
         plan_data = {
-            "name": plan_doc.name,
-            "plan_name": plan_doc.plan_name,
-            "plan_monthly_subscription": plan_doc.plan_monthly_subscription,
-            "plan_description": plan_doc.plan_description,
-            "apps": plan_doc.plan_apps
+            "name": plan.name,
+            "plan_name": plan.plan_name,
+            "plan_monthly_subscription": plan.plan_monthly_subscription,
+            "plan_description": plan.plan_description,
+            "apps": []
         }
+        
+        # Add apps if they exist
+        if hasattr(plan_doc, 'plan_apps'):
+            plan_data['apps'] = [
+                {"app_name": app.app_name} 
+                for app in plan_doc.plan_apps
+            ]
+        
         plan_docs.append(plan_data)
         
     return plan_docs
@@ -59,7 +72,7 @@ def create_subscription():
             "sub_domain": subdomain,
             "subscription_type": "Trial",  # You can modify this based on your requirements
             "plan": plan_name,
-            "start_date": date.today(),
+            "start_date": frappe.utils.today(),
             "status": "Draft",
             "created_by": user,
             "creation_date": frappe.utils.now(),
